91c2f2084d1a8dd2033d39d2520059ec6bf384ce
IT21314742 <it21314742@my.sliit.lk>
2025-01-18 22:42:50 +0530

    Update
    Update

--- watch.py
+++ watch.py
@@ -2,3 +2,7 @@ import sys
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, )
 
 from PyQt5.QtCore import QTimer, QTime, Qt
+
+class Stopwatch(QWidget):
+    def fname(arg):
+        pass
