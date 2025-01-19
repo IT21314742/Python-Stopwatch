b00c87f16658cda08e0a53c3cdb9247884b6154a
IT21314742 <it21314742@my.sliit.lk>
2025-01-19 22:28:30 +0530

    Update
    Update

--- watch.py
+++ watch.py
@@ -52,6 +52,7 @@ class Stopwatch(QWidget):
         self.start_button.clicked.connect(self.start)
         self.stop_button.clicked.connect(self.stop)
         self.reset_button.clicked.connect(self.reset)
+        self.timer.timeout.connect(self.update_display)
         
 
     def start(self):
