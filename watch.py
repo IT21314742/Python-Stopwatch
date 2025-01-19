ee108b0c08dba7516cc59aea63c82b6081ce5767
IT21314742 <it21314742@my.sliit.lk>
2025-01-19 22:24:43 +0530

    Update
    Update

--- watch.py
+++ watch.py
@@ -38,7 +38,7 @@ class Stopwatch(QWidget):
         self.setStyleSheet("""
             QpushButton, QLabel{
                 padding: 20px;
-                font-weight}
+                font-weight: bold;}
             QpushButton{
                 font-size: 50px;
                 background-color: hsl(200, 100%, 85%);
